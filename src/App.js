3abc589b1f20d6d7c5281e21ac0e2389fe8bb7ba
Lindy Merwin <lindymerwin@Lindys-Air.attlocal.net>
2021-01-23 20:16:36 -0800

    Tasks Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import { useState } from 'react'
+
 import Header from './components/Header'
+import Tasks from './components/Tasks'
+
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+    text: 'walk the dog',
+    id: 1,
+    day: 'Feb 5th at 2:30pm',
+    reminder:  false,
+    }])
 
   const name = 'Lindy';
   return (
     <div className="container">
       <Header />
+
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
